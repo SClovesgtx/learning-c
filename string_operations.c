67e52d27756cae29347b945dd6f76ede45ed9c1c
Cloves Paiva <cloves.paiva@extremedigital.com.br>
2023-01-18 17:49:02 -0300

    string

--- string_operations.c
+++ string_operations.c
@@ -7,27 +7,46 @@ int main(){
 
     char copy[6];
 
+    // COPY
     // will put 'str' content into 'copy'
     strcpy(copy, str); 
 
     printf("%s\n", copy);
 
+    // SIZE
     int str_size = strlen(str);
 
     printf("The word %s has %d characters.\n", str, str_size);
 
     char str2[6] = "World";
 
+    // CONCATENATE
     strcat(str, str2);
 
     printf("%s\n", str);
 
     char *p;
 
-    // will return the address of charcter at the str 
+    // SEARCH CHARACTER
+    // will return the address of character at the str 
     p = strchr(str, 'o');
 
     printf("%c\n", *p);
+
+    // SEARCH SUBSTRING
+    char *pos;
+    pos = strstr(str, "World");
+
+    printf("%s\n", pos);
+
+    // COMPARAISON
+    char str3[4] = "abc";
+    char str4[4] = "abc";
+
+    if(strcmp(str3, str4) == 0)
+        printf("It's equal!\n");
+    else
+        printf("It's different!");
 
     return 0;
 }
